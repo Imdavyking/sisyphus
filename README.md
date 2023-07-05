1d3ba9ee953c3c2a6d07e0ecbd7512339cab7b24
DavyKing <yungdynamic53@gmail.com>
2023-07-05 19:12:10 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# sisyphus
+
+A Flutter app showcasing interactive trading view and order books interface powered by Binance's WebSockets API.
+
+## Video Demo
+
+https://github.com/Crazelu/sisyphus/assets/58946834/9614740d-3f9a-41b8-bd59-9fa13c3c70e1
+
+
---
+++ lib/presentation/views/trade_details/widgets/app_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:sissyphus/presentation/app_assets.dart';
+import 'package:sissyphus/presentation/theme/theme_mode_selector.dart';
+import 'package:sissyphus/presentation/views/trade_details/widgets/popup_menu_button.dart';
+import 'package:sissyphus/presentation/widgets/custom_icon.dart';
+
+class CustomAppBar extends AppBar {
+  final Color color;
+  final VoidCallback? onMenuPressed;
+  final String logoPath;
+
+  CustomAppBar({
+    super.key,
+    required this.color,
+    required this.logoPath,
+    this.onMenuPressed,
+    super.elevation = 0,
+  }) : super(
+          backgroundColor: color,
+          leadingWidth: 129,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: CustomIcon(
+              iconPath: logoPath,
+              height: 34,
+              width: 121,
+            ),
+          ),
+          actions: [
+            const CustomIcon(
+              iconPath: AppAssets.avatar,
+              height: 32,
+              width: 32,
+            ),
+            const Gap(8),
+            const CustomIcon(
+              iconPath: AppAssets.globe,
+              height: 24,
+              width: 24,
+              onPressed: ThemeModeSelector.toggleMode,
+            ),
+            const Gap(8),
+            const CustomPopupMenuButton(),
+            const Gap(8),
+          ],
+        );
+}
